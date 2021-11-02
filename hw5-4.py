c3320083a16507d4e23a129e4aef5e68b41aef93
p22jdiao <p22jdiao@fairfieldprep.org>
2021-11-02 10:17:31 -0400

    finished

---
+++ hw5-4.py
@@ -0,0 +1,34 @@
+# Author: JD 11/02/2021
+
+# Question 1
+
+print("Phthon"[2])
+
+# Question 2
+
+print("Strings are simply a sequence of characters."["Strings are simply a sequence of characters.".find("o")])
+
+# Question 3
+
+print(len("excellent"))
+
+# Question 4
+
+print("apple" in "Pineapple")
+
+# Question 5
+
+print("Peach" > "pear")
+
+# Question 6
+
+print("x" * 100)
+
+# Question 7
+str(54321)
+
+# Question 8
+print(not("p" in "Pineapple"))
+
+# Question 9
+print('"Livin' + "' On a Prayer" + '" is my friend'+"'s favorite song.")
